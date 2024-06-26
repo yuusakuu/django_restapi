72190bfec272feb9e0673d0c13f4d3929745eb7e
yuusakuu <dohjeong0109@gmail.com>
2024-06-26 10:12:18 +0900

    update

---
+++ myproject/rest_api/urls.py
@@ -0,0 +1,9 @@
+#from django.conf.urls import url 
+from rest_api import views 
+from django.urls import re_path
+
+urlpatterns = [ 
+    re_path(r'^api/tutorials$', views.tutorial_list),
+    re_path(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
+    re_path(r'^api/tutorials/published$', views.tutorial_list_published)
+]
